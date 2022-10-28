69fc64c05d84ed5e04da90e5f525a2a972329687
vini00784 <vo70346@gmail.com>
2022-10-28 08:50:58 -0300

    setting float button

--- app/src/main/java/br/senai/sp/jandira/agenda/ui/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/agenda/ui/MainActivity.kt
@@ -1,12 +1,24 @@
 package br.senai.sp.jandira.agenda.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.senai.sp.jandira.agenda.R
+import br.senai.sp.jandira.agenda.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fbNewContact.setOnClickListener {
+            val openNewContactActivity = Intent(this, NewContactActivity::class.java)
+            startActivity(openNewContactActivity)
+        }
     }
 }
